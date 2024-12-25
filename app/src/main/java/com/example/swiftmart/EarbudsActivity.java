52fb14137e1f502b36999a94886cc349163554d7
hp <akbariprince648@gmail.com>
2024-12-25 12:08:07 +0530

    earbuds add

--- app/src/main/java/com/example/swiftmart/EarbudsActivity.java
+++ app/src/main/java/com/example/swiftmart/EarbudsActivity.java
@@ -12,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class EarbudsActivity extends AppCompatActivity {
-    TextView earbudstext;
+    TextView earbudstext,earbudstext1;
     LinearLayout earbudsdetailes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +20,12 @@ public class EarbudsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_earbuds);
 
         earbudstext=findViewById(R.id.earbudstext);
+        earbudstext1=findViewById(R.id.earbudstext1);
         earbudsdetailes=findViewById(R.id.earbudsdetailes);
 
 
         earbudstext.setPaintFlags(earbudstext.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        earbudstext1.setPaintFlags(earbudstext1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
 
 
 
