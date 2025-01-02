db823b35829a4956a35dfc9d59a8b50719fd8955
hp <akbariprince648@gmail.com>
2025-01-02 12:30:45 +0530

    some changes

---
+++ app/src/main/java/com/example/swiftmart/Adapter/TvSliderAdapter.java
@@ -0,0 +1,51 @@
+package com.example.swiftmart.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.swiftmart.R;
+
+import java.util.List;
+
+public class TvSliderAdapter extends RecyclerView.Adapter<TvSliderAdapter.SliderViewHolder> {
+
+    private List<Integer> imageList; // List of drawable image resources
+    private Context context;
+
+    public TvSliderAdapter(Context context, List<Integer> imageList) {
+        this.context = context;
+        this.imageList = imageList;
+    }
+
+    @NonNull
+    @Override
+    public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.mobileslider_layout, parent, false);
+        return new SliderViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
+        holder.imageView.setImageResource(imageList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageList.size();
+    }
+
+    static class SliderViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
