7420103263c8fa8baf80b51cfa6eafc0c9f659b8
hp <akbariprince648@gmail.com>
2025-01-03 10:54:18 +0530

    language screen add

--- app/src/main/java/com/example/swiftmart/Leptop_Activity.java
+++ app/src/main/java/com/example/swiftmart/Leptop_Activity.java
@@ -1,9 +1,11 @@
 package com.example.swiftmart;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Leptop_Activity extends AppCompatActivity {
     ImageView backleptop;
+    LinearLayout hplogo;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,20 @@ public class Leptop_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_leptop);
 
         backleptop=findViewById(R.id.backleptop);
+        hplogo=findViewById(R.id.hplogo);
 
         backleptop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+        hplogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(Leptop_Activity.this,Language_Screen.class);
+                startActivity(i);
             }
         });
     }
