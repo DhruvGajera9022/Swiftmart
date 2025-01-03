2bfa8e8347f699217d5c572d80aee31b56058983
hp <akbariprince648@gmail.com>
2025-01-03 16:48:12 +0530

    language screen add

--- app/src/main/java/com/example/swiftmart/Leptop_Activity.java
+++ app/src/main/java/com/example/swiftmart/Leptop_Activity.java
@@ -1,48 +1,27 @@
 package com.example.swiftmart;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Leptop_Activity extends AppCompatActivity {
     ImageView backleptop;
     LinearLayout hplogo;
-    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leptop);
 
-        backleptop=findViewById(R.id.backleptop);
-        hplogo=findViewById(R.id.hplogo);
+        backleptop = findViewById(R.id.backleptop);
+        hplogo = findViewById(R.id.hplogo);
 
-        backleptop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        backleptop.setOnClickListener(v -> onBackPressed());
 
-        hplogo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i =new Intent(Leptop_Activity.this,Language_Activity.class);
-                startActivity(i);
-            }
-        });
-    }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 }
