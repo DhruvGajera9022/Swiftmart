64aa66a72dd7602bbb37875061777afc3de8c638
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-01-17 11:44:13 +0530

    Add category screens

---
+++ app/src/main/java/com/example/swiftmart/CategoryScreen/CameraActivity.java
@@ -0,0 +1,20 @@
+package com.example.swiftmart.CategoryScreen;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.swiftmart.R;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
